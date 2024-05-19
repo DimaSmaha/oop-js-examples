af785e31c2d316997cf366f70741393bc0bd2414
DimaSmaha <dmytrosmaha@gmail.com>
2024-05-19 12:00:20 +0300

    comment

--- Inheritance/examples.ts
+++ Inheritance/examples.ts
@@ -4,3 +4,8 @@ let person = new ChildClass("Dima", 20);
 
 person.showAge();
 person.showName();
+
+/**
+ * Since we inherit the parentClass via childClass, we are able
+ * to get the methods of the parentClass using childClass *
+ */
